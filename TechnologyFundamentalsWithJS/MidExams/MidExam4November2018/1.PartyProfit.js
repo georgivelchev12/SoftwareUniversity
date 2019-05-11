51955fc7702e1f267e5c4bad81bd24e023c2bb67
Georgi <g.velchev12@gmail.com>
2019-05-11 11:46:26 +0300

    first problem solution

---
+++ TechnologyFundamentalsWithJS/MidExams/MidExam4November2018/1.PartyProfit.js
@@ -0,0 +1,31 @@
+function partyProfit(input) {
+    let partySize = +input[0];
+    let days = +input[1];
+    let coins = 0;
+    for (let i = 1; i <= days; i++) {
+        if (i%10===0) {
+            partySize-=2
+        }
+        if (i%15===0) {
+            partySize+=5;
+        }
+        coins+=50;
+        coins-=2*partySize;
+
+
+        if (i%3===0) {
+            coins-=3*partySize;
+        }
+        if (i%5===0) {
+            coins+=20*partySize;
+            if (i%3 === 0) {
+                coins-=2*partySize;
+            }
+        }
+     
+    }
+    coins/=partySize;
+    console.log(`${partySize} companions received ${Math.floor(coins)} coins each.`);
+    
+}
+partyProfit([3,5]);
