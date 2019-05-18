e8bdcd2eb28377d4d3089a3c121cd942663c7052
Georgi <g.velchev12@gmail.com>
2019-05-18 10:34:04 +0300

    add tasks

---
+++ TechnologyFundamentalsWithJS/TextProcessingAndRegularExpressions/Lab/2.Concatenation.js
@@ -0,0 +1,9 @@
+function solve(input) {
+    let output = '';
+    for (let i = 0; i < input.length; i++) {
+        output+=input[i];
+    }
+    console.log(output);
+    
+}
+solve(["First","Second","Third"])
---
+++ TechnologyFundamentalsWithJS/TextProcessingAndRegularExpressions/Lab/3.Substring.js
@@ -0,0 +1,5 @@
+function solve(str,startIndex,count) {
+    let result = str.substr(startIndex,count);
+    console.log(result);
+}
+solve("ASentance", 1, 8)
