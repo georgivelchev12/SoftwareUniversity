9ef49e7ff4b3539189a5da6db67ed18072779c3e
Georgi <g.velchev12@gmail.com>
2019-05-20 12:31:05 +0300

    add models and controllers

---
+++ WebProjectsSoftUni/Phonebook-Updated-MongoDB/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const routing = require('./routing');
+const port = 3000;
+const app = express();
+const env = process.env.NODE_ENV || 'development';
+
+const config = require('./controllers/config')[env];
+require('./controllers/database')(config);
+
+app.engine(".hbs", handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+routing(app);
+
+app.listen(port, console.log("Listening on port " + port + "..."));
