14be4df965985d701275cda1cb8afcd65cad45f5
Georgi <g.velchev12@gmail.com>
2019-05-24 09:15:22 +0300

    add solution

---
+++ TechnologyFundamentalsWithJS/MidExams/MidExam02March2019/2.BreadFactory 100 out of 100.js
@@ -0,0 +1,60 @@
+function solve(arr) {
+    let events = arr[0].split('|');
+    let energy = 100;
+    let coins = 100;
+    let currEnergy;
+    for (let i = 0; i < events.length; i++) {
+        let splited = events[i].split('-');
+        let command = splited[0];
+        let number = +splited[1];
+
+        switch (command) {
+            case 'rest':
+                currEnergy = energy;
+                energy += number;
+                if (energy < 100) {
+                    console.log(`You gained ${number} energy.`);
+                    console.log(`Current energy: ${energy}.`);
+
+                }
+                else {
+
+                    console.log(`You gained ${100 - currEnergy} energy.`);
+                    console.log(`Current energy: 100.`);
+                    energy = 100;
+                }
+                break;
+            case 'order':
+                if (energy >= 30) {
+                    console.log(`You earned ${number} coins.`);
+                    coins += number;
+                    energy -= 30;
+
+                }
+                else {
+                    energy += 50;
+                    console.log(`You had to rest!`);
+                }
+                break;
+            default:
+                if (coins - number > 0) {
+                    console.log(`You bought ${command}.`);
+                    coins -= number;
+                }
+                else {
+                    console.log(`Closed! Cannot afford ${command}.`);
+                    return;
+                }
+                break;
+        }
+    }
+    console.log(`Day completed!`);
+    console.log(`Coins: ${coins}`);
+    console.log(`Energy: ${energy}`);
+}
+
+
+solve(['rest-2|order-10|eggs-100|rest-10']);
+
+
+// solve(['order-10|order-10|order-10|flour-100|order-100|oven-100|order-1000'])
---
+++ TechnologyFundamentalsWithJS/MidExams/MidExam10March2019Group2/2.SeizeTheFire 100 out of 100.js
@@ -0,0 +1,57 @@
+function solve(input) {
+    let effort = 0;
+    let cells = [];
+    let totalFire = 0;
+    let splited = input.shift().split('#');
+    let amountOfWater = input.shift();
+    for (let i = 0; i < splited.length; i++) {
+
+        let [command, value] = splited[i].split(' = ')
+        value = +value;
+        if (amountOfWater < value) {
+            continue;
+        }
+        switch (command) {
+            case 'High':
+                if (value >= 81 && value <= 125) {
+                    cells.push(value);
+                    amountOfWater -= value;
+                    effort += (value * 0.25);
+                    totalFire += value;
+                }
+                break;
+
+            case 'Medium':
+                if (value >= 51 && value <= 80) {
+                    cells.push(value);
+                    amountOfWater -= value;
+                    effort += (value * 0.25);
+                    totalFire += value;
+                }
+                break;
+
+            case 'Low':
+                if (value >= 1 && value <= 50) {
+                    cells.push(value);
+                    amountOfWater -= value;
+                    effort += (value * 0.25);
+                    totalFire += value;
+                }
+                break;
+        }
+    }
+    console.log(`Cells:`)
+    for (let cell of cells) {
+        console.log(` - ${cell}`);
+    }
+    console.log(`Effort: ${effort.toFixed(2)}`);
+    console.log(`Total Fire: ${totalFire}`);
+
+
+}
+solve(['High = 150#Low = 55#Medium = 86#Low = 40#High = 110#Medium = 77',
+    220])
+
+    // solve(['High = 89#Meduim = 53#Low = 28#Medium = 77#Low = 23',
+    //     1250
+    //     ])
