c91cfda6e9f94d463bbd1d2ca5c210ae3ccef0bb
Georgi <g.velchev12@gmail.com>
2019-05-31 20:14:07 +0300

    add solution

---
+++ TechnologyFundamentalsWithJS/FinalExams/TechnologyFundamentalsFinal Exam14April2019GroupI/1.ArrivingInKathmandu.js
@@ -0,0 +1,36 @@
+function solve(input) {
+    let pattern = /[a-z0-9]+/gi;
+    for (let i = 0; i < input.length; i++) {
+        let splited = input[i].split(/[!@#$?]/g).join('').split('=').join(' ').split('<<').join(' ');
+        if (splited == 'Last note') {
+            break;
+        }
+        let [name,length,code] = splited.split(' ');
+        length=+length
+        if (code != undefined
+        && length == code.length 
+        && (name.match(pattern) == name)) {
+            console.log(`Coordinates found! ${name} -> ${code}`);
+            
+        }
+        else{
+            console.log(`Nothing found!`);
+        }
+    }
+}
+
+solve(['!@Ma?na?sl!u@=7<<tv58ycb4845',
+    'E!ve?rest=.6<<tuvz26',
+'!K@2.,##$=4<<tvnd',
+    '!Shiha@pan@gma##9<<tgfgegu67',
+    '!###Anna@pur@na##=16<<tv5dekdz8x11ddkc',
+    'Last note']
+    )
+
+
+// solve(['Ka?!#nch@@en@ju##nga@=3<<thfbghvn',
+//     '=9Cho?@#Oyu<<thvb7ydht',
+//     'Nan??ga#Par!ba!t?=16<<twm03q2rx5hpmyr6',
+//     'Dhau??la#gi@ri?!#=3<<bvnfhrtiuy',
+//     'Last note']
+//     )
