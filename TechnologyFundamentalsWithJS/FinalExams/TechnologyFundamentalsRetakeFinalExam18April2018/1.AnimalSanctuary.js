dc005735067fbba999705811544a58b3f9e3a40a
Georgi <g.velchev12@gmail.com>
2019-06-17 16:31:08 +0300

    add solution

--- TechnologyFundamentalsWithJS/FinalExams/TechnologyFundamentalsRetakeFinalExam18April2018/1.AnimalSanctuary.js
+++ TechnologyFundamentalsWithJS/FinalExams/TechnologyFundamentalsRetakeFinalExam18April2018/1.AnimalSanctuary.js
@@ -1,32 +1,30 @@
 function solve(arr) {
-    let linesNumber = Number(arr.shift());
-    let lines = arr;
+    let linesNumber = Number(arr[0]);
     let weightOfAnimals = 0;
-   for (let i = 0; i < linesNumber; i++) {
-       let pattern = /^n:(?<name>[^;]+);t:(?<kind>[^;]+);c--(?<country>[A-Za-z ]+)$/g;
-       if ((validLine = pattern.exec(lines[i])) !== null) {
+    for (let i = 1; i <= linesNumber; i++) {
+        let pattern = /^n:(?<name>[^;]+);t:(?<kind>[^;]+);c--(?<country>[A-Za-z ]+)$/g;
+        let validLine;
+        if ((validLine = pattern.exec(arr[i])) !== null) {
             let nameCode = validLine.groups['name'];
             let kindCode = validLine.groups['kind'];
             let country = validLine.groups['country'];
-            let name = nameCode.match(/[A-Za-z ]+/g).join('');
-            let kind = kindCode.match(/[A-Za-z ]+/g).join('');
-            weightOfAnimals += nameCode.replace(/[^0-9]/g,'')
-                .split('')
-                .map(Number)
-                .reduce((a,b) => a+b,0);
-            weightOfAnimals += kindCode.replace(/[^0-9]/g,'')
-                .split('')
-                .map(Number)
-                .reduce((a,b) => a+b,0);
-           console.log(`${name} is a ${kind} from ${country}`);
+            let name = nameCode.match(/[a-z\s]+/gi).join('');
+            let kind = kindCode.match(/[a-z\s]+/gi).join('');
+            weightOfAnimals += Sum(nameCode) + Sum(kindCode);
+            console.log(`${name} is a ${kind} from ${country}`);
         }
     }
     console.log(`Total weight of animals: ${weightOfAnimals}KG`);
+    function Sum(weight) {
+        return weight.replace(/[^0-9]/g, '')
+            .split('')
+            .map(Number)
+            .reduce((a, b) => a + b, 0);
+    }
 }
-solve([ '4',
-    'n:Bo^%4b35454bie#$;t:Ele5ph#$34a%nt;c--Africa',
-    'n:Honey;t:Ti^^5ger;c--India',
-    'bla;t:1234a;c--America',
-    'n:A#$@545n;t:Cat241$@#23;cGermany' ]
 
+solve(['3',
+    'n:M5%ar4#le@y;t:B3#e!!a2#2r;c--Australia',
+    'n:G3e%6org43e;t:C€$at2%;c--Africa',
+    'n:AlicE:Won;c-India']
 );
---
+++ TechnologyFundamentalsWithJS/FinalExams/TechnologyFundamentalsRetakeFinalExam18April2018/2.FeedTheAnimals.js
@@ -0,0 +1,74 @@
+function feedTheAnimals(arr) {
+    let animals = [];
+    arr.forEach((element) => {
+        let animalObj = {};
+        let [command, animalName, food, area] = element.split(':');
+        food = +food;
+        switch (command) {
+            case 'Add':
+                if (!animals.find(x => x['name'] === animalName)) {
+                    animalObj['name'] = animalName;
+                    animalObj['food'] = food;
+                    animalObj['area'] = area;
+                    animals.push(animalObj);
+                } else {
+                    let animalNameFinder = animals.find(x => x['name'] === animalName);
+                    if (animalNameFinder) {
+                        let addFood = animalNameFinder['food'];
+                        addFood += food;
+                        animalNameFinder['food'] = addFood;
+                    }
+                }
+                break;
+            case 'Feed':
+                let animalNameFind = animals.find(x => x['name'] === animalName);
+                if (animalNameFind) {
+                    let indexOfFeededAnimal = animals.indexOf(animalNameFind);
+                    let subtractFood = animalNameFind['food'];
+                    subtractFood -= food;
+                    animalNameFind['food'] = subtractFood;
+                    if (subtractFood <= 0) {
+                        animals.splice(indexOfFeededAnimal, 1);
+                        console.log(`${animalName} was successfully fed`);
+                    }
+                }
+                break;
+            case 'Last Info':
+                printResult(animals);
+                break;
+        }
+    });
+
+    function printResult(arr) {
+        arr.sort((a, b) => b['food'] === a['food']
+            ? a['name'].localeCompare(b['name'])
+            : b['food'] - a['food']);
+        let noFedAreaArr = [];
+        console.log('Animals:');
+        for (let arrElement of arr) {
+            console.log(arrElement['name'] + ' -> ' + arrElement['food'] + 'g');
+            noFedAreaArr.push(arrElement['area']);
+        }
+        let count = {};
+        noFedAreaArr.forEach(x => count[x] = (count[x] || 0) + 1);
+        console.log(`Areas with hungry animals:`);
+        count = Object.entries(count)
+            .sort((a, b) => b[1] === a[1] ? b[0].localeCompare(a[0]) : b[1] > a[1]);
+        for (let countElement of count) {
+            console.log(`${countElement[0]} : ${countElement[1]}`);
+        }
+    }
+}
+
+feedTheAnimals([ 'Add:Maya:7600:WaterfallArea',
+    'Add:Bobbie:6570:DeepWoodsArea',
+    'Add:Adam:4500:ByTheCreek',
+    'Add:Jamie:1290:RiverArea',
+    'Add:Gem:8730:WaterfallArea',
+    'Add:Maya:1230:WaterfallArea',
+    'Add:Jamie:560:RiverArea',
+    'Feed:Bobbie:6300:DeepWoodsArea',
+    'Feed:Adam:4650:ByTheCreek',
+    'Feed:Jamie:2000:RiverArea',
+    'Last Info' ]
+);
