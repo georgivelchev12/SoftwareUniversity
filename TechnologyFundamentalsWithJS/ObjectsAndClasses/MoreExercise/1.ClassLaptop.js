3259527f6e678d0dd80fbc011dab4a8a04af67cf
Georgi <g.velchev12@gmail.com>
2019-06-24 00:31:08 +0300

    add task

--- TechnologyFundamentalsWithJS/ObjectsAndClasses/MoreExercise/1.ClassLaptop.js
+++ TechnologyFundamentalsWithJS/ObjectsAndClasses/MoreExercise/1.ClassLaptop.js
@@ -1,36 +1,37 @@
-function solve() {
-
-    class Laptop {
-        constructor(info, quality) {
-            this.info = info;
-            this.quality = quality;
-            this.price = 0;
-        }
-        turnOn() {
+class Laptop {
+    constructor(info, quality) {
+        this.info = info;
+        this.quality = +quality;
+        this.price = (800 - Number(this.info.age) * 2 + this.quality * 0.5);
+        this.isOn = false;
+    }
+    turnOn() {
+        if (!this.isOn) {
             this.isOn = true;
             this.quality--;
-            this.price = 800 - info.age * 2 + this.quality * 0.5;
         }
-        turnOff() {
+        this.price = (800 - Number(this.info.age) * 2 + this.quality * 0.5);
+        
+      }
+    turnOff() {
+        if (this.isOn) {
             this.isOn = false;
             this.quality--;
-            this.price = 800 - info.age * 2 + this.quality * 0.5;
         }
-        showInfo() {
-            this.price = 800 - info.age * 2 + this.quality * 0.5;
-            let jsonFormat = JSON.stringify(this.info);
-            return jsonFormat;
-        }
+        this.price = (800 - Number(this.info.age) * 2 + this.quality * 0.5);
+        
     }
-    let info = { producer: "Dell", age: 2, brand: "XPS" }
-    let laptop = new Laptop(info, 10)
-
-    laptop.turnOn()
-    console.log(laptop.showInfo())
-    laptop.turnOff()
-    console.log(laptop.quality)
-    laptop.turnOn()
-    console.log(laptop.isOn)
-    console.log(laptop.price)
+    showInfo() { 
+        let jsonFormat = JSON.stringify(this.info);
+        return jsonFormat;
+    }
 }
-solve();
+info = { producer: "Dell", age: 2, brand: "XPS" }
+let laptop = new Laptop(info, 10)
+laptop.turnOn()
+console.log(laptop.showInfo())
+laptop.turnOff()
+console.log(laptop.quality)
+laptop.turnOn()
+console.log(laptop.isOn)
+console.log(laptop.price)
