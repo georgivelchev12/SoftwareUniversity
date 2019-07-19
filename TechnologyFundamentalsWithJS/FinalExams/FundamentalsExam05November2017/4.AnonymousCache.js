182a17a5c8c675063b613cfd0959f544702ae6e2
Georgi <g.velchev12@gmail.com>
2019-07-19 12:31:11 +0300

    add solution

---
+++ TechnologyFundamentalsWithJS/FinalExams/FundamentalsExam05November2017/4.AnonymousCache.js
@@ -0,0 +1,11 @@
+solve = (arr) => {
+
+
+}
+solve(['Users',
+    'BankAccounts',
+    'ADDB444 -> 23111 | BankAccounts',
+    'Students -> 2000 | Users',
+    'Workers -> 24233 | Users',
+    'thetinggoesskrra',
+    ''])
